98af658f5a4d8bfdaa67d6bf49fba92aec3a426e
dani <groddpapyrus@yahoo.com>
2020-02-17 10:22:06 +0500

    website

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu");
+const gnav = document.getElementById("great-nav");
+
+const getIt = () => {
+	const dropdown = document.getElementById("dropdown");
+	
+	dropdown.style.display = "block";
+}
+
+const dontGetIt = () => {
+	dropdown.style.display = "none";
+}
+
+gnav.onmouseover = getIt;
+gnav.onmouseout = dontGetIt;
