1fe398950001bf26c8f2e19cea232aec2c3d8e48
dani <groddpapyrus@yahoo.com>
2020-02-18 10:34:16 +0500

    website

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const menu = document.getElementById("menu");
 const gnav = document.getElementById("great-nav");
+const dropdown = document.getElementById("dropdown");
 
 const getIt = () => {
-	const dropdown = document.getElementById("dropdown");
-	
 	dropdown.style.display = "block";
 }
 
@@ -13,3 +12,14 @@ const dontGetIt = () => {
 
 gnav.onmouseover = getIt;
 gnav.onmouseout = dontGetIt;
+
+/*if (gnav.onmouseover) {
+	dropdown.style.display = "block";
+}*/
+/*else {
+	dropdown.style.display = "block";
+}*/
+
+/*input[type="radio"]:checked ~ .tick,
+input[type="radio"]:checked ~ .cross {
+  visibility: visible;*/
