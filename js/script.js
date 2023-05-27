618b1c3711f8a3b42a3e8cf4ff48311921d19706
Danicore <50729913+Danicore@users.noreply.github.com>
2023-05-27 18:12:21 +0100

    Tried dropdown toggle again
    Tried dropdown toggle again. Only problem remaining is if statement condition executes instead of the block.

--- js/script.js
+++ js/script.js
@@ -4,19 +4,17 @@ const dropdown = document.getElementById("dropdown");
 const portrait = document.getElementById("portrait");
 
 const getIt = () => {
-	dropdown.classList.remove("block");
-}
-
-const dontGetIt = () => {
 	dropdown.classList.add("block");
 }
 
-//const change = document.onclick;
-menu.onclick = getIt;
-document.onclick = dontGetIt;
+const dontGetIt = () => {
+	dropdown.classList.remove("block");
+}
 
-if (menu.onclick === true) {
-	getIt;
+menu.onclick = getIt;
+
+if (dropdown.style.display === "block") {
+	document.onclick = dontGetIt;
 }
 
 // else {
@@ -26,6 +24,8 @@ if (menu.onclick === true) {
 //menu.onclick = getIt;
 
 //dropdown.style.display = "none"
+
+//var change = document.onclick;
 
 //menu.onmouseover = getIt;
 //menu.onmouseout = dontGetIt;
